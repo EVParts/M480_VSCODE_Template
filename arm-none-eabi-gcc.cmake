6c4ed19d2a28c65e47575ffc49f51b594e19fafd
MacDue <macdue@dueutil.tech>
2022-06-22 14:22:14 +0100

    Run cmake-format

--- arm-none-eabi-gcc.cmake
+++ arm-none-eabi-gcc.cmake
@@ -1,11 +1,11 @@
 # the file from
 # https://github.com/ErichStyger/mcuoneclipse/blob/master/Examples/VisualStudioCode/FRDM-K22F/FRDM-K22F_Simple/arm-none-eabi-gcc.cmake
-# ref. https://mcuoneclipse.com/2021/05/04/visual-studio-code-for-c-c-with-arm-cortex-m-part-2/
+# ref.
+# https://mcuoneclipse.com/2021/05/04/visual-studio-code-for-c-c-with-arm-cortex-m-part-2/
 
 #
-# For using Ninja:
-# cmake -DCMAKE_MAKE_PROGRAM=ninja.exe -DCMAKE_TOOLCHAIN_FILE="arm-none-eabi-gcc.cmake" -G "Ninja" ..
-# followed by
+# For using Ninja: cmake -DCMAKE_MAKE_PROGRAM=ninja.exe
+# -DCMAKE_TOOLCHAIN_FILE="arm-none-eabi-gcc.cmake" -G "Ninja" .. followed by
 # 'ninja' or 'cmake --build .' to build it
 
 set(CMAKE_SYSTEM_NAME Generic)
@@ -18,18 +18,22 @@ set(TOOLCHAIN_PREFIX ${ARM_TOOLCHAIN_DIR}/arm-none-eabi-)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-if (WIN32)
-    set(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}gcc.exe")
-    set(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}g++.exe")
+if(WIN32)
+  set(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}gcc.exe")
+  set(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}g++.exe")
 else()
-    set(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}gcc")
-    set(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}g++")
+  set(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}gcc")
+  set(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}g++")
 endif()
 
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
 
-set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy CACHE INTERNAL "objcopy tool")
-set(CMAKE_SIZE_UTIL ${TOOLCHAIN_PREFIX}size CACHE INTERNAL "size tool")
+set(CMAKE_OBJCOPY
+    ${TOOLCHAIN_PREFIX}objcopy
+    CACHE INTERNAL "objcopy tool")
+set(CMAKE_SIZE_UTIL
+    ${TOOLCHAIN_PREFIX}size
+    CACHE INTERNAL "size tool")
 
 set(CMAKE_FIND_ROOT_PATH ${BINUTILS_PATH})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
--- lwip.cmake
+++ lwip.cmake
@@ -1,4 +1,3 @@
-
 add_library(lwip INTERFACE)
 
 set(LWIP_DIR ${CMAKE_CURRENT_SOURCE_DIR}/ThirdParty/LwIP)
@@ -6,24 +5,18 @@ set(FREERTOS_DIR ${CMAKE_CURRENT_LIST_DIR}/ThirdParty/FreeRTOS)
 set(LWIP_INCLUDE_DIRS
     # BSP
     ${CMAKE_SOURCE_DIR}/inc
-	${CMAKE_SOURCE_DIR}/port/lwip/include
+    ${CMAKE_SOURCE_DIR}/port/lwip/include
     # FREERTOS
     ${FREERTOS_DIR}/portable/GCC/ARM_CM4F
     ${FREERTOS_DIR}/portable/MemMang
     ${FREERTOS_DIR}/include
     # LWIP
-    ${LWIP_DIR}/src/include
-    )
+    ${LWIP_DIR}/src/include)
 
-set(LWIP_COMPILER_FLAGS  ${DEFINE_COMPILE_FLAGS})
+set(LWIP_COMPILER_FLAGS ${DEFINE_COMPILE_FLAGS})
 
 include(${LWIP_DIR}/src/Filelists.cmake)
 
-target_link_libraries(lwip INTERFACE
-    lwipcore
-    lwipallapps
-)
+target_link_libraries(lwip INTERFACE lwipcore lwipallapps)
 
-target_include_directories(lwip INTERFACE
-    ${LWIP_INCLUDE_DIRS}
-)
+target_include_directories(lwip INTERFACE ${LWIP_INCLUDE_DIRS})
