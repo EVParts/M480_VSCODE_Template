27b7376b779a6cbabb61f865978f9a8f8e4a43fa
MacDue <macdue@dueutil.tech>
2022-06-22 14:14:12 +0100

    Remove windows specific build settings
    It is now possible to compile this demo on Linux.

--- arm-none-eabi-gcc.cmake
+++ arm-none-eabi-gcc.cmake
@@ -1,8 +1,8 @@
-# the file from 
+# the file from
 # https://github.com/ErichStyger/mcuoneclipse/blob/master/Examples/VisualStudioCode/FRDM-K22F/FRDM-K22F_Simple/arm-none-eabi-gcc.cmake
 # ref. https://mcuoneclipse.com/2021/05/04/visual-studio-code-for-c-c-with-arm-cortex-m-part-2/
 
-# 
+#
 # For using Ninja:
 # cmake -DCMAKE_MAKE_PROGRAM=ninja.exe -DCMAKE_TOOLCHAIN_FILE="arm-none-eabi-gcc.cmake" -G "Ninja" ..
 # followed by
@@ -11,16 +11,22 @@
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 
-set(ARM_TOOLCHAIN_DIR "C:/Program Files (x86)/GNU Arm Embedded Toolchain/10 2021.10/bin")
-set(BINUTILS_PATH ${ARM_TOOLCHAIN_DIR}) 
+set(ARM_TOOLCHAIN_DIR ${CMAKE_SOURCE_DIR}/Toolchain/bin)
+set(BINUTILS_PATH ${ARM_TOOLCHAIN_DIR})
 
 set(TOOLCHAIN_PREFIX ${ARM_TOOLCHAIN_DIR}/arm-none-eabi-)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
-set(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}gcc.exe")
+if (WIN32)
+    set(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}gcc.exe")
+    set(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}g++.exe")
+else()
+    set(CMAKE_C_COMPILER "${TOOLCHAIN_PREFIX}gcc")
+    set(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}g++")
+endif()
+
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
-set(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}g++.exe")
 
 set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy CACHE INTERNAL "objcopy tool")
 set(CMAKE_SIZE_UTIL ${TOOLCHAIN_PREFIX}size CACHE INTERNAL "size tool")
